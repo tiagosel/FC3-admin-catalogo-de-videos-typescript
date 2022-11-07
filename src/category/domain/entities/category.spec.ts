bae38ee229f470b3a19995264b9368020faf099b
TIAGO BORGES <tiago@DESKTOP-D2NTHE8.localdomain>
2022-11-07 15:42:50 -0300

    Teste constructor da entidade categoria

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,25 @@
 import { Category } from "./category";
 describe("Category Unit Tests", () => {
-    test("constructor of category", () => {
-        const category:Category = new Category("Movie");
-        expect(category.name).toBe("Movie");
-    })
-        
+  test("constructor of category", () => {
+    const created_at = new Date();
+    const props = {
+      name: "Movie",
+      description: "description",
+      is_active: true,
+      created_at: created_at
+    };
+    const category: Category = new Category(props);
+
+    //expect(category.props.name).toBe("Movie");
+    //expect(category.props.description).toBe("description");
+    //expect(category.props.is_active).toBeTruthy();
+    //expect(category.props.created_at).toBe(created_at);
+
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "description",
+      is_active: true,
+      created_at: created_at
+    });
+  });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,34 @@
-export class Category {
-    constructor(public name:string){}
+export type CategoryProperties ={
+   name: string,
+   description?: string,
+   is_active?: boolean,
+   created_at?: Date
 }
+export class Category {
+  constructor(public readonly props: CategoryProperties) {}
+
+  get name() {
+    return this.props.name;
+  }
+
+
+
+  get description() {
+    return this.props.description;
+  }
+
+
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+
+
+  get created_at() {
+    return this.props.created_at;
+  }
+  
+}
+
+const category: Category = new Category({name:'Movie'});
